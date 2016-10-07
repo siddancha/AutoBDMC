b3401c3b700bb1332e8e73118d9b77bdceea15a4
Siddharth Ancha <siddancha@gmail.com>
2016-10-07 13:42:29 -0400

    mf: Report in terms of Jeffreys divergence + x-axis in logscale

--- experiments/mf/scripts/plot.py
+++ experiments/mf/scripts/plot.py
@@ -11,16 +11,25 @@ def main():
 	c_dict = read_files_dict("experiments/mf/results/output_c", sort="steps")
 	uc_dict = {key:uc_dict[key][:(uc_trunc if uc_trunc != 0 else len(uc_dict[key]))] for key in uc_dict}
 	
-	plt.errorbar(uc_dict["aisSteps"], uc_dict["aisMeans"], np.array([uc_dict["aisLowers"], uc_dict["aisUppers"]]), c='red', label='ucAIS')
-	plt.errorbar(uc_dict["raisSteps"], uc_dict["raisMeans"], np.array([uc_dict["raisLowers"], uc_dict["raisUppers"]]), c='darkred', label='ucRAIS')
-	plt.errorbar(c_dict["aisSteps"], c_dict["aisMeans"], np.array([c_dict["aisLowers"], c_dict["aisUppers"]]), c='blue', label='cAIS')
-	plt.errorbar(c_dict["raisSteps"], c_dict["raisMeans"], np.array([c_dict["raisLowers"], c_dict["raisUppers"]]), c='darkblue', label='cRAIS')
+	ucDivSteps = uc_dict["aisSteps"]
+	ucDivMeans = uc_dict["raisMeans"] - uc_dict["aisMeans"]
+	ucDivLowers = uc_dict["raisLowers"] + uc_dict["aisUppers"]
+	ucDivUppers = uc_dict["raisUppers"] + uc_dict["aisLowers"]
+	plt.errorbar(ucDivSteps, ucDivMeans, np.array([ucDivLowers, ucDivUppers]), c='red', label='Uncollapsed')
+
+	cDivSteps = c_dict["aisSteps"]
+	cDivMeans = c_dict["raisMeans"] - c_dict["aisMeans"]
+	cDivLowers = c_dict["raisLowers"] + c_dict["aisUppers"]
+	cDivUppers = c_dict["raisUppers"] + c_dict["aisLowers"]
+	plt.errorbar(cDivSteps, cDivMeans, np.array([cDivLowers, cDivUppers]), c='blue', label='Collapsed')
+		
 	plt.xlabel("HMC/No-U-Turn Steps")
-	plt.ylabel("Marginal Likelihood (nats)")
+	plt.ylabel("Jeffreys Divergence (nats)")
 	plt.title("Collapsed vs Uncollapsed Matrix Factorization - Steps")
-	plt.legend(loc="lower right")
-	all_steps = np.concatenate([uc_dict["aisSteps"], uc_dict["raisSteps"], c_dict["aisSteps"], c_dict["raisSteps"]])
-	plt.xlim(*pretty_xlim(min(all_steps), max(all_steps)))
+	plt.legend(loc="upper right")
+	all_steps = np.concatenate([ucDivSteps, cDivSteps])
+	plt.xscale('log')
+	plt.xlim(*pretty_xlim(min(all_steps), max(all_steps), scale='log'))
 	plt.savefig('experiments/mf/plots/steps.pdf', format='pdf', dpi=1000)
 	plt.clf()
 	
@@ -28,16 +37,25 @@ def main():
 	c_dict = read_files_dict("experiments/mf/results/output_c", sort="times")
 	c_dict = {key:c_dict[key][:(c_trunc if c_trunc != 0 else len(c_dict[key]))] for key in c_dict}
 
-	plt.errorbar(uc_dict["aisTimes"], uc_dict["aisMeans"], np.array([uc_dict["aisLowers"], uc_dict["aisUppers"]]), c='red', label='ucAIS')
-	plt.errorbar(uc_dict["raisTimes"], uc_dict["raisMeans"], np.array([uc_dict["raisLowers"], uc_dict["raisUppers"]]), c='darkred', label='ucRAIS')
-	plt.errorbar(c_dict["aisTimes"], c_dict["aisMeans"], np.array([c_dict["aisLowers"], c_dict["aisUppers"]]), c='blue', label='cAIS')
-	plt.errorbar(c_dict["raisTimes"], c_dict["raisMeans"], np.array([c_dict["raisLowers"], c_dict["raisUppers"]]), c='darkblue', label='cRAIS')
+	ucDivTimes = uc_dict["aisTimes"] + uc_dict["raisTimes"]
+	ucDivMeans = uc_dict["raisMeans"] - uc_dict["aisMeans"]
+	ucDivLowers = uc_dict["raisLowers"] + uc_dict["aisUppers"]
+	ucDivUppers = uc_dict["raisUppers"] + uc_dict["aisLowers"]
+	plt.errorbar(ucDivTimes, ucDivMeans, np.array([ucDivLowers, ucDivUppers]), c='red', label='Uncollapsed')
+
+	cDivTimes = c_dict["aisTimes"] + c_dict["raisTimes"]
+	cDivMeans = c_dict["raisMeans"] - c_dict["aisMeans"]
+	cDivLowers = c_dict["raisLowers"] + c_dict["aisUppers"]
+	cDivUppers = c_dict["raisUppers"] + c_dict["aisLowers"]
+	plt.errorbar(cDivTimes, cDivMeans, np.array([cDivLowers, cDivUppers]), c='blue', label='Collapsed')
+
 	plt.xlabel("Time (in sec)")
-	plt.ylabel("Marginal Likelihood (nats)")
+	plt.ylabel("Jeffreys Divergence (nats)")
 	plt.title("Collapsed vs Uncollapsed Matrix Factorization - Time")
-	plt.legend(loc="lower right")
-	all_times = np.concatenate([uc_dict["aisTimes"], uc_dict["raisTimes"], c_dict["aisTimes"], c_dict["raisTimes"]])
-	plt.xlim(*pretty_xlim(min(all_times), max(all_times)))
+	plt.legend(loc="upper right")
+	all_times = np.concatenate([ucDivTimes, cDivTimes])
+	plt.xscale('log')
+	plt.xlim(*pretty_xlim(min(all_times), max(all_times), scale='log'))
 	plt.savefig('experiments/mf/plots/times.pdf', format='pdf', dpi=1000)
 
 if __name__ == '__main__':
