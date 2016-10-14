f72c84cbd3c61f2818c166421e1cf199584e7feb
Siddharth Ancha <siddancha@gmail.com>
2016-10-13 23:01:38 -0400

    Complete mf/stan-webbpl - add plotting & figures

--- experiments/mf/stan-webppl/scripts/plot.py
+++ experiments/mf/stan-webppl/scripts/plot.py
@@ -10,67 +10,60 @@ def main():
 	stan_uc_dict = read_files_dict("experiments/mf/stan-webppl/results/stan/output_uc", sort="times", lang='stan')
 	stan_c_dict = read_files_dict("experiments/mf/stan-webppl/results/stan/output_c", sort="times", lang='stan')
 
-	# ucDivSteps = uc_dict["aisSteps"]
-	# ucDivMeans = uc_dict["raisMeans"] - uc_dict["aisMeans"]
-	# ucDivLowers = uc_dict["raisLowers"] + uc_dict["aisUppers"]
-	# ucDivUppers = uc_dict["raisUppers"] + uc_dict["aisLowers"]
-	# plt.errorbar(ucDivSteps, ucDivMeans, np.array([ucDivLowers, ucDivUppers]), c='red', label='Uncollapsed')
-
-	# cDivSteps = c_dict["aisSteps"]
-	# cDivMeans = c_dict["raisMeans"] - c_dict["aisMeans"]
-	# cDivLowers = c_dict["raisLowers"] + c_dict["aisUppers"]
-	# cDivUppers = c_dict["raisUppers"] + c_dict["aisLowers"]
-	# plt.errorbar(cDivSteps, cDivMeans, np.array([cDivLowers, cDivUppers]), c='blue', label='Collapsed')
-		
-	# plt.xlabel("HMC/No-U-Turn Steps")
-	# plt.ylabel("Jeffreys Divergence (nats)")
-	# plt.title("Collapsed vs Uncollapsed Matrix Factorization - Steps")
-	# plt.legend(loc="upper right")
-	# all_steps = np.concatenate([ucDivSteps, cDivSteps])
-	# plt.xscale('log')
-	# plt.xlim(*pretty_xlim(min(all_steps), max(all_steps), scale='log'))
-	# plt.savefig('experiments/mf/stan/plots/steps.pdf', format='pdf', dpi=1000)
-	# plt.clf()
-	
-	# uc_dict = read_files_dict("experiments/mf/stan/results/output_uc", sort="times")
-	# c_dict = read_files_dict("experiments/mf/stan/results/output_c", sort="times")
-	# c_dict = {key:c_dict[key][:(c_trunc if c_trunc != 0 else len(c_dict[key]))] for key in c_dict}
-
 	plt.errorbar(stan_uc_dict["aisTimes"], stan_uc_dict["aisMeans"], np.array([stan_uc_dict["aisLowers"], stan_uc_dict["aisUppers"]]), c='red', label='Stan (uncollapsed)')
 	plt.errorbar(stan_uc_dict["raisTimes"], stan_uc_dict["raisMeans"], np.array([stan_uc_dict["raisLowers"], stan_uc_dict["raisUppers"]]), c='red')
 
-	plt.errorbar(stan_c_dict["aisTimes"], stan_c_dict["aisMeans"], np.array([stan_c_dict["aisLowers"], stan_c_dict["aisUppers"]]), c='darkred', label='Stan (collapsed)')
-	plt.errorbar(stan_c_dict["raisTimes"], stan_c_dict["raisMeans"], np.array([stan_c_dict["raisLowers"], stan_c_dict["raisUppers"]]), c='darkred')
+	plt.errorbar(stan_c_dict["aisTimes"], stan_c_dict["aisMeans"], np.array([stan_c_dict["aisLowers"], stan_c_dict["aisUppers"]]), c='maroon', label='Stan (collapsed)')
+	plt.errorbar(stan_c_dict["raisTimes"], stan_c_dict["raisMeans"], np.array([stan_c_dict["raisLowers"], stan_c_dict["raisUppers"]]), c='maroon')
 
-	plt.errorbar(webppl_uc_dict["aisTimes"], webppl_uc_dict["aisMeans"], np.array([webppl_uc_dict["aisLowers"], webppl_uc_dict["aisUppers"]]), c='blue', label='Webppl (uncollapsed)')
-	plt.errorbar(webppl_uc_dict["raisTimes"], webppl_uc_dict["raisMeans"], np.array([webppl_uc_dict["raisLowers"], webppl_uc_dict["raisUppers"]]), c='blue')
+	plt.errorbar(webppl_uc_dict["aisTimes"], webppl_uc_dict["aisMeans"], np.array([webppl_uc_dict["aisLowers"], webppl_uc_dict["aisUppers"]]), c='dodgerblue', label='WebPPL (uncollapsed)')
+	plt.errorbar(webppl_uc_dict["raisTimes"], webppl_uc_dict["raisMeans"], np.array([webppl_uc_dict["raisLowers"], webppl_uc_dict["raisUppers"]]), c='dodgerblue')
 
-	plt.errorbar(webppl_c_dict["aisTimes"], webppl_c_dict["aisMeans"], np.array([webppl_c_dict["aisLowers"], webppl_c_dict["aisUppers"]]), c='darkblue', label='Webppl (collapsed)')
+	plt.errorbar(webppl_c_dict["aisTimes"], webppl_c_dict["aisMeans"], np.array([webppl_c_dict["aisLowers"], webppl_c_dict["aisUppers"]]), c='darkblue', label='WebPPL (collapsed)')
 	plt.errorbar(webppl_c_dict["raisTimes"], webppl_c_dict["raisMeans"], np.array([webppl_c_dict["raisLowers"], webppl_c_dict["raisUppers"]]), c='darkblue')
 
-	plt.legend()
-	plt.show()
+	plt.xlabel("Time (in sec)")
+	plt.ylabel("Marginal Log Likelihood (nats)")
+	plt.title("Collapsed and Uncollapsed Matrix Factorization in Stan and WebPPL", fontsize=12)
+	plt.legend(loc='lower right', prop={'size': 12})
+	all_times = np.concatenate([d[c] for d in [stan_c_dict, stan_uc_dict, webppl_c_dict, webppl_uc_dict] for c in ["aisTimes", "raisTimes"]])
+	plt.xscale('log')
+	plt.xlim(*pretty_xlim(min(all_times), max(all_times), scale='log'))
+	plt.savefig('experiments/mf/stan-webppl/plots/bounds.pdf')
+	plt.clf()
 
-	# ucDivTimes = uc_dict["aisTimes"] + uc_dict["raisTimes"]
-	# ucDivMeans = uc_dict["raisMeans"] - uc_dict["aisMeans"]
-	# ucDivLowers = uc_dict["raisLowers"] + uc_dict["aisUppers"]
-	# ucDivUppers = uc_dict["raisUppers"] + uc_dict["aisLowers"]
-	# plt.errorbar(ucDivTimes, ucDivMeans, np.array([ucDivLowers, ucDivUppers]), c='red', label='Uncollapsed')
+	ucDivStanTimes = stan_uc_dict["aisTimes"] + stan_uc_dict["raisTimes"]
+	ucDivStanMeans = stan_uc_dict["raisMeans"] - stan_uc_dict["aisMeans"]
+	ucDivStanLowers = stan_uc_dict["raisLowers"] + stan_uc_dict["aisUppers"]
+	ucDivStanUppers = stan_uc_dict["raisUppers"] + stan_uc_dict["aisLowers"]
+	plt.errorbar(ucDivStanTimes, ucDivStanMeans, np.array([ucDivStanLowers, ucDivStanUppers]), c='red', label='Stan (uncollapsed)')
 
-	# cDivTimes = c_dict["aisTimes"] + c_dict["raisTimes"]
-	# cDivMeans = c_dict["raisMeans"] - c_dict["aisMeans"]
-	# cDivLowers = c_dict["raisLowers"] + c_dict["aisUppers"]
-	# cDivUppers = c_dict["raisUppers"] + c_dict["aisLowers"]
-	# plt.errorbar(cDivTimes, cDivMeans, np.array([cDivLowers, cDivUppers]), c='blue', label='Collapsed')
+	cDivStanTimes = stan_c_dict["aisTimes"] + stan_c_dict["raisTimes"]
+	cDivStanMeans = stan_c_dict["raisMeans"] - stan_c_dict["aisMeans"]
+	cDivStanLowers = stan_c_dict["raisLowers"] + stan_c_dict["aisUppers"]
+	cDivStanUppers = stan_c_dict["raisUppers"] + stan_c_dict["aisLowers"]
+	plt.errorbar(cDivStanTimes, cDivStanMeans, np.array([cDivStanLowers, cDivStanUppers]), c='maroon', label='Stan (collapsed)')
 
-	# plt.xlabel("Time (in sec)")
-	# plt.ylabel("Jeffreys Divergence (nats)")
-	# plt.title("Collapsed vs Uncollapsed Matrix Factorization - Time")
-	# plt.legend(loc="upper right")
-	# all_times = np.concatenate([ucDivTimes, cDivTimes])
-	# plt.xscale('log')
-	# plt.xlim(*pretty_xlim(min(all_times), max(all_times), scale='log'))
-	# plt.savefig('experiments/mf/stan/plots/times.pdf', format='pdf', dpi=1000)
+	ucDivWebpplTimes = webppl_uc_dict["aisTimes"] + webppl_uc_dict["raisTimes"]
+	ucDivWebpplMeans = webppl_uc_dict["raisMeans"] - webppl_uc_dict["aisMeans"]
+	ucDivWebpplLowers = webppl_uc_dict["raisLowers"] + webppl_uc_dict["aisUppers"]
+	ucDivWebpplUppers = webppl_uc_dict["raisUppers"] + webppl_uc_dict["aisLowers"]
+	plt.errorbar(ucDivWebpplTimes, ucDivWebpplMeans, np.array([ucDivWebpplLowers, ucDivWebpplUppers]), c='dodgerblue', label='WebPPL (uncollapsed)')
+
+	cDivWebpplTimes = webppl_c_dict["aisTimes"] + webppl_c_dict["raisTimes"]
+	cDivWebpplMeans = webppl_c_dict["raisMeans"] - webppl_c_dict["aisMeans"]
+	cDivWebpplLowers = webppl_c_dict["raisLowers"] + webppl_c_dict["aisUppers"]
+	cDivWebpplUppers = webppl_c_dict["raisUppers"] + webppl_c_dict["aisLowers"]
+	plt.errorbar(cDivWebpplTimes, cDivWebpplMeans, np.array([cDivWebpplLowers, cDivWebpplUppers]), c='darkblue', label='WebPPL (collapsed)')
+
+	plt.xlabel("Time (in sec)")
+	plt.ylabel("Jeffreys Divergence (nats)")
+	plt.title("Collapsed and Uncollapsed Matrix Factorization in Stan and WebPPL", fontsize=12)
+	plt.legend(loc='upper right', prop={'size': 12})
+	all_steps = np.concatenate([ucDivStanTimes, cDivStanTimes, ucDivWebpplTimes, cDivWebpplTimes])
+	plt.xscale('log')
+	plt.xlim(*pretty_xlim(min(all_steps), max(all_steps), scale='log'))
+	plt.savefig('experiments/mf/stan-webppl/plots/gaps.pdf', format='pdf', dpi=1000)
 
 if __name__ == '__main__':
 	main()
