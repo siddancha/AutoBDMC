27c4d46f1a5094bd34f2ae3db0964090234b0a8a
Siddharth Ancha <siddancha@gmail.com>
2016-10-25 17:19:17 -0400

    Add mf/stan-webppl figures where x-axes show steps

--- experiments/mf/stan-webppl/scripts/plot.py
+++ experiments/mf/stan-webppl/scripts/plot.py
@@ -29,7 +29,7 @@ def main():
 	all_times = np.concatenate([d[c] for d in [stan_c_dict, stan_uc_dict, webppl_c_dict, webppl_uc_dict] for c in ["aisTimes", "raisTimes"]])
 	plt.xscale('log')
 	plt.xlim(*pretty_xlim(min(all_times), max(all_times), scale='log'))
-	plt.savefig('experiments/mf/stan-webppl/plots/bounds.pdf')
+	plt.savefig('experiments/mf/stan-webppl/plots/bounds-times.pdf')
 	plt.clf()
 
 	ucDivStanTimes = stan_uc_dict["aisTimes"] + stan_uc_dict["raisTimes"]
@@ -60,11 +60,77 @@ def main():
 	plt.ylabel("Jeffreys Divergence (nats)")
 	plt.title("Collapsed and Uncollapsed Matrix Factorization in Stan and WebPPL", fontsize=12)
 	plt.legend(loc='lower left', prop={'size': 12})
-	all_steps = np.concatenate([ucDivStanTimes, cDivStanTimes, ucDivWebpplTimes, cDivWebpplTimes])
+	all_times = np.concatenate([ucDivStanTimes, cDivStanTimes, ucDivWebpplTimes, cDivWebpplTimes])
+	plt.xscale('log')
+	plt.xlim(*pretty_xlim(min(all_times), max(all_times), scale='log'))
+	plt.yscale('log')
+	plt.savefig('experiments/mf/stan-webppl/plots/gaps-times.pdf', format='pdf', dpi=1000)
+	plt.clf()
+
+	webppl_uc_dict = read_files_dict("experiments/mf/stan-webppl/results/webppl/output_uc", sort="steps", lang='webppl')
+	webppl_c_dict = read_files_dict("experiments/mf/stan-webppl/results/webppl/output_c", sort="steps", lang='webppl')
+	stan_uc_dict = read_files_dict("experiments/mf/stan-webppl/results/stan/output_uc", sort="steps", lang='stan')
+	stan_c_dict = read_files_dict("experiments/mf/stan-webppl/results/stan/output_c", sort="steps", lang='stan')
+
+	plt.errorbar(stan_uc_dict["aisSteps"], stan_uc_dict["aisMeans"], np.array([stan_uc_dict["aisLowers"], stan_uc_dict["aisUppers"]]), c='red', label='Stan (uncollapsed)')
+	plt.errorbar(stan_uc_dict["raisSteps"], stan_uc_dict["raisMeans"], np.array([stan_uc_dict["raisLowers"], stan_uc_dict["raisUppers"]]), c='red')
+
+	plt.errorbar(stan_c_dict["aisSteps"], stan_c_dict["aisMeans"], np.array([stan_c_dict["aisLowers"], stan_c_dict["aisUppers"]]), c='maroon', label='Stan (collapsed)')
+	plt.errorbar(stan_c_dict["raisSteps"], stan_c_dict["raisMeans"], np.array([stan_c_dict["raisLowers"], stan_c_dict["raisUppers"]]), c='maroon')
+
+	plt.errorbar(webppl_uc_dict["aisSteps"], webppl_uc_dict["aisMeans"], np.array([webppl_uc_dict["aisLowers"], webppl_uc_dict["aisUppers"]]), c='dodgerblue', label='WebPPL (uncollapsed)')
+	plt.errorbar(webppl_uc_dict["raisSteps"], webppl_uc_dict["raisMeans"], np.array([webppl_uc_dict["raisLowers"], webppl_uc_dict["raisUppers"]]), c='dodgerblue')
+
+	plt.errorbar(webppl_c_dict["aisSteps"], webppl_c_dict["aisMeans"], np.array([webppl_c_dict["aisLowers"], webppl_c_dict["aisUppers"]]), c='darkblue', label='WebPPL (collapsed)')
+	plt.errorbar(webppl_c_dict["raisSteps"], webppl_c_dict["raisMeans"], np.array([webppl_c_dict["raisLowers"], webppl_c_dict["raisUppers"]]), c='darkblue')
+
+	plt.xlabel("HMC/No-U-Turn Steps")
+	plt.ylabel("Marginal Log Likelihood (nats)")
+	plt.title("Collapsed and Uncollapsed Matrix Factorization in Stan and WebPPL", fontsize=12)
+	plt.legend(loc='lower right', prop={'size': 12})
+	all_steps = np.concatenate([d[c] for d in [stan_c_dict, stan_uc_dict, webppl_c_dict, webppl_uc_dict] for c in ["aisSteps", "raisSteps"]])
+	plt.xscale('log')
+	plt.xlim(*pretty_xlim(min(all_steps), max(all_steps), scale='log'))
+	plt.savefig('experiments/mf/stan-webppl/plots/bounds-steps.pdf')
+	plt.clf()
+
+	assert np.all(stan_uc_dict["aisSteps"] == stan_uc_dict["raisSteps"])
+	ucDivStanSteps = stan_uc_dict["aisSteps"]
+	ucDivStanMeans = stan_uc_dict["raisMeans"] - stan_uc_dict["aisMeans"]
+	ucDivStanLowers = stan_uc_dict["raisLowers"] + stan_uc_dict["aisUppers"]
+	ucDivStanUppers = stan_uc_dict["raisUppers"] + stan_uc_dict["aisLowers"]
+	plt.errorbar(ucDivStanSteps, ucDivStanMeans, np.array([ucDivStanLowers, ucDivStanUppers]), c='red', label='Stan (uncollapsed)')
+
+	assert np.all(stan_c_dict["aisSteps"] == stan_c_dict["raisSteps"])
+	cDivStanSteps = stan_c_dict["aisSteps"]
+	cDivStanMeans = stan_c_dict["raisMeans"] - stan_c_dict["aisMeans"]
+	cDivStanLowers = stan_c_dict["raisLowers"] + stan_c_dict["aisUppers"]
+	cDivStanUppers = stan_c_dict["raisUppers"] + stan_c_dict["aisLowers"]
+	plt.errorbar(cDivStanSteps, cDivStanMeans, np.array([cDivStanLowers, cDivStanUppers]), c='maroon', label='Stan (collapsed)')
+
+	assert np.all(webppl_uc_dict["aisSteps"] == webppl_uc_dict["raisSteps"])
+	ucDivWebpplSteps = webppl_uc_dict["aisSteps"]
+	ucDivWebpplMeans = webppl_uc_dict["raisMeans"] - webppl_uc_dict["aisMeans"]
+	ucDivWebpplLowers = webppl_uc_dict["raisLowers"] + webppl_uc_dict["aisUppers"]
+	ucDivWebpplUppers = webppl_uc_dict["raisUppers"] + webppl_uc_dict["aisLowers"]
+	plt.errorbar(ucDivWebpplSteps, ucDivWebpplMeans, np.array([ucDivWebpplLowers, ucDivWebpplUppers]), c='dodgerblue', label='WebPPL (uncollapsed)')
+
+	assert np.all(webppl_c_dict["aisSteps"] == webppl_c_dict["raisSteps"])
+	cDivWebpplSteps = webppl_c_dict["aisSteps"]
+	cDivWebpplMeans = webppl_c_dict["raisMeans"] - webppl_c_dict["aisMeans"]
+	cDivWebpplLowers = webppl_c_dict["raisLowers"] + webppl_c_dict["aisUppers"]
+	cDivWebpplUppers = webppl_c_dict["raisUppers"] + webppl_c_dict["aisLowers"]
+	plt.errorbar(cDivWebpplSteps, cDivWebpplMeans, np.array([cDivWebpplLowers, cDivWebpplUppers]), c='darkblue', label='WebPPL (collapsed)')
+
+	plt.xlabel("HMC/No-U-Turn Steps")
+	plt.ylabel("Jeffreys Divergence (nats)")
+	plt.title("Collapsed and Uncollapsed Matrix Factorization in Stan and WebPPL", fontsize=12)
+	plt.legend(loc='lower left', prop={'size': 12})
+	all_steps = np.concatenate([ucDivStanSteps, cDivStanSteps, ucDivWebpplSteps, cDivWebpplSteps])
 	plt.xscale('log')
 	plt.xlim(*pretty_xlim(min(all_steps), max(all_steps), scale='log'))
 	plt.yscale('log')
-	plt.savefig('experiments/mf/stan-webppl/plots/gaps.pdf', format='pdf', dpi=1000)
+	plt.savefig('experiments/mf/stan-webppl/plots/gaps-steps.pdf', format='pdf', dpi=1000)
 
 if __name__ == '__main__':
 	main()
