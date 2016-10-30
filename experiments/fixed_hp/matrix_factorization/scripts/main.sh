219c9c03eefa1f227efea16b0bf037f25201c10a
Siddharth Ancha <siddancha@gmail.com>
2016-10-30 15:12:41 -0400

    Add fixed_hp/matrix_factorization experiment

--- experiments/fixed_hp/matrix_factorization/scripts/main.sh
+++ experiments/fixed_hp/matrix_factorization/scripts/main.sh
@@ -1,14 +1,12 @@
 # User options.
-GEN_DATA_ARGS="50 5 25"
+GEN_DATA_ARGS="50 5 50"
+STEPS_LIST=( 10 20 50 100 200 400 700 1000 1400 1800 2000 )
+SAMPLES=100
 BURN_LIST=( 10 100 1000 10000 )
-STEPS_LIST=( 10 30 100 300 1000 2000 4000 6000 )
-SAMPLES=50
 WARMUP=1000
 SEED=1
 
 # Subexperiment options.
-SUBEXPERIMENT=matrix_factorization
-MODEL=uncollapsed_hp
-FIXED_HP_MODEL=uncollapsed_fixed_hp
+MODEL=matrix_factorization
 
 source experiments/fixed_hp/__common/main.sh
